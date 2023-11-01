d43292b6f94b35acb89dd6ac2fc2994b0aa21524
alanguilhermeM <alanguilherme13@outlook.com>
2023-11-01 13:57:17 -0300

    Finalizado requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,22 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+        return
+
+    file_name, file_extension = os.path.splitext(path_file)
+
+    if not file_extension == '.txt':
+        sys.stderr.write('Formato inválido\n')
+        return
+
+    linhas_do_arquivo = []
+    with open(path_file, "r") as arquivo:
+        for linha in arquivo:
+            linhas_do_arquivo.append(linha.strip())
+
+    return linhas_do_arquivo
+# print(txt_importer('statics/arquivo_teste.txt'))
