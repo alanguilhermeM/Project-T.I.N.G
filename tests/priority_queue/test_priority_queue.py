4952ad75c2c686cbd0e67b27536e07afb7155dbf
alanguilhermeM <alanguilherme13@outlook.com>
2023-11-06 12:47:54 -0300

    FInalizado requisito 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,31 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    project = PriorityQueue()
+
+    mock1 = {
+        "qtd_linhas": 3
+    }
+
+    mock2 = {
+        "qtd_linhas": 7
+    }
+
+    project.enqueue(mock2)
+    assert len(project.regular_priority) == 1
+
+    project.enqueue(mock1)
+    assert len(project.high_priority) == 1
+
+    assert project.search(0) == mock1
+    assert project.dequeue() == mock1
+
+    project.dequeue()
+    assert len(project.regular_priority) == 0
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        project.search(2)
+
+    
