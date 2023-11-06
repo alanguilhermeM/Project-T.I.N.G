1df69888d7d9c31c5f06d7037f6debfd6df9aaf6
alanguilhermeM <alanguilherme13@outlook.com>
2023-11-06 15:31:11 -0300

    Finalizado req 7 e 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,42 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    matching_files = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        occurrences = []
+        for i, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": i + 1})
+
+        if occurrences:
+            matching_files.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+
+    return matching_files
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    matching_files = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        occurrences = []
+        for i, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": i + 1, "conteudo": line})
+
+        if occurrences:
+            matching_files.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+
+    return matching_files
+
